4be49b4094f0f1305561a7359221e0359542be3f
ndjordjevic <ndjordjevic@gmail.com>
2019-10-15 20:37:21 +0200

    basic client

---
+++ cmd/client/client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/micro/go-micro"
+)
+import hello "github.com/ndjordjevic/go-sb-micro/api/rpc/proto"
+
+func main() {
+	// create a new service
+	service := micro.NewService()
+
+	// parse command line flags
+	service.Init()
+
+	// Use the generated client stub
+	cl := hello.NewSayService("go.micro.srv.greeter", service.Client())
+
+	// Make request
+	rsp, err := cl.Hello(context.Background(), &hello.Request{
+		Name: "John",
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(rsp.Msg)
+}
